553c754a5ab2c78b7ea6fdd0eef7e312d1f3e2ca
Marine LAPORTE <marinelaporte@live.fr>
2020-11-30 14:13:52 +0100

    pas fini

--- myscraper.php
+++ myscraper.php
@@ -5,7 +5,6 @@ include "email_scraper.php";
 //$url = 'https://github.com/nyxgeek/username-lists/blob/master/usernames-top100/usernames_gmail.com.txt';
 
 
-
 if (isset($_POST['submit'])){
     if (!empty($_POST['url'])){
         $url = htmlspecialchars($_POST['url']);
@@ -14,12 +13,12 @@ if (isset($_POST['submit'])){
             $database->insert("Emails", [
                 "email"=>$email,
                 ]);
+                $data = $database->select("Emails", "*");
+                //echo display_table();
         };
 
     }
 }
-
-
 
 ?>
 <!DOCTYPE html>
@@ -28,14 +27,39 @@ if (isset($_POST['submit'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scraper Emails</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
     <form action="" method="POST">
 
     <label for="url">Enter an https:// URL:</label>
     <input type="url" name="url" id="url" placeholder="https://example.com" pattern="https://.*" size="30" required>
-    
+
     <input type="submit" name="submit" value="envoyer">
     </form>
+
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+                <table class="table">
+                    <thead>
+                        <th>Voici la liste de tout les emails inscrits.</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach($data as $email){
+                            while ($a <= 10) {
+                                # code...
+                            }?>
+                        <tr>
+                            <td><?php $email?></td>
+                        </tr>
+                        <?php }?>
+
+                    </tbody>
+                </table>
+            </section>
+
+        </div>
+    </main>
 </body>
 </html>
